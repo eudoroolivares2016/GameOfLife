6aaf67013d6d3bf0d0bf5c7fb8762c4c0413495f
Ed A Olivares <34591886+eudoroolivares2016@users.noreply.github.com>
2021-07-30 23:24:08 -0500

    Add files via upload
    Added more functionality to Grid to set up the rules for the game of Life

--- Grid.py
+++ Grid.py
@@ -16,4 +16,31 @@ class Grid:
         for x in range(self.rows):
             for y in range(self.columns):
                 self.grid_array[x][y] = random.randint(0,1)
-                
+
+    def Conway(self,off_color,on_color,surface)::
+        for x in range(self.rows):
+            for y in range(self.columns):
+                y_pos = y * self.scale
+                x_pos = x * self.scale
+                if self.grid_array[x][y] == 1:
+                    pygame.draw.rect(surface,on_color,[x_pos,y_pos,self.scale - self.offset, self.scale - self.offset])
+            next = np.ndarray(shape = (self.size))
+            for x in range(self.rows):
+                for y in range(self.columns):
+                    state = state.grid_array[x][y]
+                    neighbors = self.get_neighbors(x,y)
+                    if state == 0 and neighbors == 3: #Any dead cell with 3 live neighbors becomes a live cell
+                        next[x][y] = 1
+                    elif state == 1 and (neighbors == 3 or neighbors == 2)
+                        next[x][y] == 1
+                    elif state == 1 and (neighbors > 3):
+                        next[x][y] == 0
+                    elif state == 1 and (neighbors < 2):
+                        next[x][y] == 0
+                    else:
+                        next[x][y] == state
+            self.next.grid_array = next
+    def get_neighbors(self, x_pos,y_pos):
+        #TODO
+
+
